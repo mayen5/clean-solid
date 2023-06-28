3c071dfd132e3720f65a8b708a6602988066dfff
cmayen <carmelo5mayen@gmail.com>
2023-06-28 12:43:41 -0600

    SOLID

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,38 @@
+interface Bird {
+    eat(): void;
+}
+
+interface FlyingBird {
+    fly(): number;
+}
+
+interface RunningBird {
+    run(): void;
+}
+
+interface SwimmerBird {
+    swim(): void;
+}
+
+class Tucan implements Bird, FlyingBird {
+    public fly() { return 50; }
+    public eat() {}
+    public run() {}
+    public swim() {}
+
+}
+
+class Humminbird implements Bird, FlyingBird {
+    public fly() { return 100; }
+    public eat() {}
+}
+
+class Ostrich implements Bird, RunningBird {
+    public eat() {}
+    public run() {}
+}
+
+class Penguin implements Bird, SwimmerBird {
+    public eat() {}
+    public swim() {}
+}
